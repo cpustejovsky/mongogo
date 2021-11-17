85a5b95edbcb310075f64012eb767e47653d1012
cpustejovsky <charles.pustejovsky@gmail.com>
2021-11-17 13:15:39 -0500

    Add function to decode json requests around user

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -12,4 +13,23 @@ func ServerError(log *log.Logger, w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	log.Error(2, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+type FormUser struct {
+	Name   string
+	Email  string
+	Active bool
+	Age    int
+	Id     string
+}
+
+func DecodeUserForm(r *http.Request) (FormUser, error) {
+	decoder := json.NewDecoder(r.Body)
+
+	var user FormUser
+	err := decoder.Decode(&user)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
 }
