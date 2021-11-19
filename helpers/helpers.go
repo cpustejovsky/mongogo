3c341a068fa00183ab8ce7cf9a1ad004502c7da7
cpustejovsky <charles.pustejovsky@gmail.com>
2021-11-18 21:30:19 -0500

    Replace user with nil in error returns; add TODO

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -19,19 +19,20 @@ func ServerError(log *log.Logger, w http.ResponseWriter, err error) {
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-//TODO: replace with generics?
+//TODO: replace with generics
 func DecodeForm(r *http.Request, form interface{}) (interface{}, error) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
-		return form, err
+		return nil, err
 	}
 	if len(body) <= 0 {
-		return form, EmptyBodyError
+		return nil, EmptyBodyError
 	}
+	fmt.Println(body)
 	err = json.Unmarshal(body, &form)
 	if err != nil {
-		return form, err
+		return nil, err
 	}
 	return form, nil
 }
